f6ff8d5c4d838f2424fbd1942dc5d96f01a7dd38
Philip Stier <pstier@elevatedthird.com>
2019-03-02 22:34:16 -0700

    chapter/04: creating dynamic components

--- index.js
+++ index.js
@@ -62,6 +62,28 @@ var app6 = new Vue({
 });
 
 
+/// Seventh app:  registering apps
+// Define a new component called todo-item
+Vue.component('todo-item', {
+  // The todo-item component now accepts a
+  // "prop", which is like a custom attribute.
+  // This prop is called todo.
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+});
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
+
+
 app.message = 'I have changed the data!';
 
 app3.seen = false;
